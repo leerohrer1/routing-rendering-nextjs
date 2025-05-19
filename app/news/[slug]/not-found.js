7a1670572d4d06d3e61f53298967af06a83c44fb
leerohrer1 <leerohrer1@gmail.com>
2025-05-19 17:37:07 -0500

    not found and parallel routes

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFound () { 
+    return (
+        <div id="error">
+            <h1>Not Found</h1>
+            <p> The requested service is down.</p>
+        </div>
+    )
+}
