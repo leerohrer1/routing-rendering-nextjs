171ee4a214fe70e5c1d8901f64fcd248304bb358
leerohrer1 <leerohrer1@gmail.com>
2025-05-19 20:07:10 -0500

    catch all fallback routing

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,54 @@
+import NewsList from "@/components/news-list";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
+import Link from "next/link";
+
+export default function FilteredNewsPage({ params }) {
+  const selectedYear = params.filter?.[0];
+  const selectedMonth = params.filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for selected period.</p>;
+
+  if (news && news.length > 0) newsContent = <NewsList news={news} />;
+
+  return (
+    <>
+      <header id="archive-header">
+        Archive Page
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}
--- app/archive/@archive/[slug]/page.js
+++
@@ -1,6 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FilteredNewsPage({ params }) {
-  return <NewsList news={getNewsForYear(params.slug)} />;
-}
